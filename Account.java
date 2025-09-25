c8582f7b4e31825ac196923da77bb1242a5392a7
WamweruKamau <167517189+WamweruKamau@users.noreply.github.com>
2025-09-25 05:11:49 +0300

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,12 @@
+package IST4070A;
+
+public class Account {
+	 int accountNo;
+	 String accountName;
+	 double balance;
+public void deposit(double amount)
+	 {
+	 	balance = balance + amount;
+	 }
+
+}
---
+++ CreateAccount.java
@@ -0,0 +1,13 @@
+package IST4070A;
+
+
+public class CreateAccount {
+public static void main(String[]args)
+{
+	Account emmaAccount = new Account();
+	emmaAccount.accountNo = 1423;
+	emmaAccount.accountName = "Emma";
+	emmaAccount.balance = 500;
+System.out.println("A/c no:"+ emmaAccount.accountNo +" "+"A/c name:"+ emmaAccount.accountName +" "+ "Balance:" + emmaAccount.balance);
+}
+}
